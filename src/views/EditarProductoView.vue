8cccc63a6475c5172681d28113843b9b2f5104d9
Antonio Romero <tonio93romero@gmail.com>
2026-03-23 15:24:05 -0500

    corregido botones de editar y crear producto

--- src/views/EditarProductoView.vue
+++ src/views/EditarProductoView.vue
@@ -59,7 +59,7 @@
             </b-form-group>
 
             <b-form-group label="Promoción:" label-for="promocion">
-             <b-form-input id="promocion" v-model="form.Promocion" type="text" placeholder="Ej: 5+1 10+2" @input="form.Promocion = $event ? $event.replace(/[^0-9+\\s]/g, '') : ''" />
+             <b-form-input id="promocion" v-model="form.Promocion" type="text" placeholder="Ej: 5+1 10+2" />
               <small class="text-muted">Formato: cantidad+bonificación (separar con espacios)</small>
             </b-form-group>
           </b-col>
